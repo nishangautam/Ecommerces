95adb3ef9efca7a4d216adbb4c840d1f60cc6a2b
nishangautam <nishan.gautam.987@gmail.com>
2021-01-24 14:08:52 +0545

    server added

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,20 @@ import data from './data';
 
 const app=express();
 
-app.get("/api/products",(req,res)=>{
+app.get("/",(req,res)=>{
+res.send('Hello World');
     res.send(data.products);
 });
-app.listen(5000,() => {console.log("server started at https://localhost:5000")});
+// app.listen(5000,() => {console.log("server started at https://localhost:5000")});
 // install babel 
+app.get('/api/products', function (req, res) {
+   
+    res.send(data.products);
+})
+
+var server = app.listen(5000, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import {BrowserRouter,Route,Link} from 'react-router-dom';
-import data from './data';
-import HomeScreen from './Screens/HomeScreen';
-import ProductScreen from './Screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   const openMenu = ()=> {
